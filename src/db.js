4236129d750f4543ddbde10b9261bf110a614a34
SAKSHAM KAMBOJ <sakshamkamboj2072@gmail.com>
2024-02-09 23:11:10 +0530

    update db

--- src/db.js
+++ src/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/URL-Shortner", {
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
